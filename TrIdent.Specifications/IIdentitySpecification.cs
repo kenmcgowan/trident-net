03a2010b787f2ed8dd5545403cadd6981364131a
kenmcgowan <ken.mcgowan@gmail.com>
2018-10-20 13:42:25 -0700

    TI-3: Changed the common IIdentity spec to return false instead of throwing exceptions on null or incompatible exceptions, fixed broken hash code spec

--- TrIdent.Specifications/IIdentitySpecification.cs
+++ TrIdent.Specifications/IIdentitySpecification.cs
@@ -5,104 +5,95 @@ using Xunit;
 
 namespace TrIdent.Specifications
 {
-    public abstract class IIdentitySpecification<TIdentity>
-        where TIdentity : IIdentity
-    {
-        #region specifications
-        [Fact]
-        public void Implementation__Implementations_provide_their_own_Object_Equals_implementation()
-        {
+   public abstract class IIdentitySpecification<TIdentity>
+       where TIdentity : IIdentity
+   {
+      #region specifications
+      [Fact]
+      public void Implementation__Implementations_provide_their_own_Object_Equals_implementation()
+      {
 
-            var sutType = typeof(TIdentity);
-            var objectEqualsMethod = sutType.GetMethod("Equals", new [] { typeof(object) });
+         var sutType = typeof(TIdentity);
+         var objectEqualsMethod = sutType.GetMethod("Equals", new[] { typeof(object) });
 
-            objectEqualsMethod.Should().NotBeNull();
-            objectEqualsMethod.Attributes.Should().HaveFlag(MethodAttributes.Public);
-            objectEqualsMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Static);
-            objectEqualsMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Abstract);
-            objectEqualsMethod.DeclaringType.Should().Be(sutType);
-        }
+         objectEqualsMethod.Should().NotBeNull();
+         objectEqualsMethod.Attributes.Should().HaveFlag(MethodAttributes.Public);
+         objectEqualsMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Static);
+         objectEqualsMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Abstract);
+         objectEqualsMethod.DeclaringType.Should().Be(sutType);
+      }
 
-        [Fact]
-        public void Implementation__Implementations_provide_their_own_Object_GetHashCode_implementation()
-        {
-            var sutType = typeof(TIdentity);
-            var objectEqualsMethod = sutType.GetMethod("GetHashCode", new [] { typeof(object) });
+      [Fact]
+      public void Implementation__Implementations_provide_their_own_Object_GetHashCode_implementation()
+      {
+         var sutType = typeof(TIdentity);
+         var getHashCodeMethod = sutType.GetMethod("GetHashCode", new Type[0]);
 
-            objectEqualsMethod.Should().NotBeNull();
-            objectEqualsMethod.Attributes.Should().HaveFlag(MethodAttributes.Public);
-            objectEqualsMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Static);
-            objectEqualsMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Abstract);
-            objectEqualsMethod.DeclaringType.Should().Be(sutType);
-        }
+         getHashCodeMethod.Should().NotBeNull();
+         getHashCodeMethod.Attributes.Should().HaveFlag(MethodAttributes.Public);
+         getHashCodeMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Static);
+         getHashCodeMethod.Attributes.Should().NotHaveFlag(MethodAttributes.Abstract);
+         getHashCodeMethod.DeclaringType.Should().Be(sutType);
+      }
 
-        [Fact]
-        public void Object_Equals__Implementations_throw_ArgumentNullException_when_obj_is_null()
-        {
-            var sut = this.CreateSubject();
-            Action invokingObjectEqualsWithNull = () => sut.Equals((object)null);
-
-            invokingObjectEqualsWithNull.Should().Throw<ArgumentNullException>();
-        }
+      [Fact]
+      public void Object_Equals__Implementations_return_false_when_obj_argument_is_null()
+      {
+         var sut = this.CreateSubject();
+         sut.Equals(null).Should().BeFalse();
+      }
 
 
-        [Fact]
-        public void Object_Equals__Implementations_throw_InvalidOperationException_when_other_differs_in_implementation_type()
-        {
-            var other = new FakeIdentity();
-            var sut = this.CreateSubject();
-            Action invokingObjectEqualsWithAlternateIdentityType = () => sut.Equals((object)other);
+      [Fact]
+      public void Object_Equals__Implementations_return_false_when_other_differs_in_implementation_type()
+      {
+         var other = new FakeIdentity();
+         var sut = this.CreateSubject();
+         sut.Equals((object)other).Should().BeFalse();
+      }
 
-            invokingObjectEqualsWithAlternateIdentityType.Should().Throw<InvalidOperationException>();
-        }
+      [Fact]
+      public void Object_Equals__Implementations_return_true_when_passed_self_same_instance()
+      {
+         var sut = this.CreateSubject();
 
-        [Fact]
-        public void Object_Equals__Implementations_return_true_when_passed_self_same_instance()
-        {
-            var sut = this.CreateSubject();
+         sut.Equals((object)sut).Should().Be(true);
+      }
 
-            sut.Equals((object)sut).Should().Be(true);
-        }
+      [Fact]
+      public void IIdentity_Equals__Implementations_return_false_when_other_is_null()
+      {
+         var sut = this.CreateSubject();
+         sut.Equals((IIdentity)null).Should().BeFalse();
+      }
 
-        [Fact]
-        public void IIdentity_Equals__Implementations_throw_ArgumentNullException_when_other_is_null()
-        {
-            var sut = this.CreateSubject();
-            Action invokingIIdentityEqualsWithNull = () => sut.Equals((IIdentity)null);
+      [Fact]
+      public void IIdentity_Equals__Implementations_return_false_when_other_differs_in_implementation_type()
+      {
+         var other = new FakeIdentity();
+         var sut = this.CreateSubject();
+         sut.Equals(other).Should().BeFalse();
+      }
 
-            invokingIIdentityEqualsWithNull.Should().Throw<ArgumentNullException>();
-        }
+      [Fact]
+      public void IIdentity_Equals__Implementations_return_true_when_passed_self_same_instance()
+      {
+         var sut = this.CreateSubject();
 
+         sut.Equals(sut).Should().Be(true);
+      }
+      #endregion
 
-        [Fact]
-        public void IIdentity_Equals__Implementations_throw_InvalidOperationException_when_other_differs_in_implementation_type()
-        {
-            var other = new FakeIdentity();
-            var sut = this.CreateSubject();
-            Action invokingIIdentityEqualsWithAlternateIdentityType = () => sut.Equals(other);
+      #region infrastructure
+      protected abstract TIdentity CreateSubject();
 
-            invokingIIdentityEqualsWithAlternateIdentityType.Should().Throw<InvalidOperationException>();
-        }
-
-        [Fact]
-        public void IIdentity_Equals__Implementations_return_true_when_passed_self_same_instance()
-        {
-            var sut = this.CreateSubject();
-
-            sut.Equals(sut).Should().Be(true);
-        }
-        #endregion
-
-        #region infrastructure
-        protected abstract TIdentity CreateSubject();
-
-        private class FakeIdentity : IIdentity
-        {
-            public bool Equals(IIdentity other)
-            {
-                throw new NotImplementedException();
-            }
-        }
-        #endregion
-    }
+      private class FakeIdentity : IIdentity
+      {
+         public bool Equals(IIdentity other)
+         {
+            throw new NotImplementedException();
+         }
+      }
+      #endregion
+   }
 }
